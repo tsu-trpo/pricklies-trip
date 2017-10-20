6ad84fee2e838e9ca0c7b693f5e41fa9fa4229ba
kiselyovanat <kiselyov-natalya@mail.ru>
2017-10-21 01:31:19 +0700

    [-] лишние enter'ы [+] недостающие пробелы в ScreenConsts.h

--- Classes/ScreenConsts.h
+++ Classes/ScreenConsts.h
@@ -7,21 +7,19 @@ inline Vec2 getLeftUpCorner()
     Vec2 visibleOrigin = Director::getInstance()->getVisibleOrigin();
     Size visibleSize = Director::getInstance()->getVisibleSize();
     return Vec2(visibleOrigin.x, visibleOrigin.y + visibleSize.height);
-
 }
 
 inline Vec2 getLeftDownCorner()
 {
     Vec2 visibleOrigin = Director::getInstance()->getVisibleOrigin();
     return Vec2(visibleOrigin.x, visibleOrigin.y);
-
 }
 
 inline Vec2 getRightUpCorner()
 {
     Vec2 visibleOrigin = Director::getInstance()->getVisibleOrigin();
     Size visibleSize = Director::getInstance()->getVisibleSize();
-    return Vec2(visibleOrigin.x+ visibleSize.width, visibleOrigin.y + visibleSize.height);
+    return Vec2(visibleOrigin.x + visibleSize.width, visibleOrigin.y + visibleSize.height);
 }
 
 
@@ -29,6 +27,6 @@ inline Vec2 getRightDownCorner()
 {
     Vec2 visibleOrigin = Director::getInstance()->getVisibleOrigin();
     Size visibleSize = Director::getInstance()->getVisibleSize();
-    return Vec2(visibleOrigin.x+ visibleSize.width, visibleOrigin.y);
+    return Vec2(visibleOrigin.x + visibleSize.width, visibleOrigin.y);
 }
 
