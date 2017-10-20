2749db825d0e1587eb861e826d7803c5a75d3b21
kiselyovanat <kiselyov-natalya@mail.ru>
2017-10-21 01:13:06 +0700

    [+] inline-функции для получения координат углов экрана

---
+++ Classes/ScreenConsts.h
@@ -0,0 +1,34 @@
+#pragma once
+
+USING_NS_CC;
+
+inline Vec2 getLeftUpCorner()
+{
+    Vec2 visibleOrigin = Director::getInstance()->getVisibleOrigin();
+    Size visibleSize = Director::getInstance()->getVisibleSize();
+    return Vec2(visibleOrigin.x, visibleOrigin.y + visibleSize.height);
+
+}
+
+inline Vec2 getLeftDownCorner()
+{
+    Vec2 visibleOrigin = Director::getInstance()->getVisibleOrigin();
+    return Vec2(visibleOrigin.x, visibleOrigin.y);
+
+}
+
+inline Vec2 getRightUpCorner()
+{
+    Vec2 visibleOrigin = Director::getInstance()->getVisibleOrigin();
+    Size visibleSize = Director::getInstance()->getVisibleSize();
+    return Vec2(visibleOrigin.x+ visibleSize.width, visibleOrigin.y + visibleSize.height);
+}
+
+
+inline Vec2 getRightDownCorner()
+{
+    Vec2 visibleOrigin = Director::getInstance()->getVisibleOrigin();
+    Size visibleSize = Director::getInstance()->getVisibleSize();
+    return Vec2(visibleOrigin.x+ visibleSize.width, visibleOrigin.y);
+}
+
