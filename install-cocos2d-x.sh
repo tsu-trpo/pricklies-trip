a2ec7e8bd3f2064d2b37de4d146f06c0df791f03
lipatova <katrinelipatova@gmail.com>
2017-10-24 00:29:42 -0400

    [+] remove sudo in 2 lines

--- install-cocos2d-x.sh
+++ install-cocos2d-x.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e
+set -x
 
 mkdir $HOME/cocos
 
@@ -9,8 +10,8 @@ unzip $HOME/cocos/cocos2d-x-3.15.1.zip -d $HOME/cocos
 rm -f $HOME/cocos/cocos2d-x-3.15.1.zip
 
 
-sudo cp $HOME/cocos/coc*/extern*/linux-specific/fmod/prebuilt/64-bit/libfmod.so /usr/lib
-sudo cp $HOME/cocos/coc*/extern*/linux-specific/fmod/prebuilt/64-bit/libfmodL.so /usr/lib
+cp $HOME/cocos/coc*/extern*/linux-specific/fmod/prebuilt/64-bit/libfmod.so /usr/lib
+cp $HOME/cocos/coc*/extern*/linux-specific/fmod/prebuilt/64-bit/libfmodL.so /usr/lib
 ln -s libfmod.so libfmod.so.6
 
 cd $HOME/cocos/cocos*/build
