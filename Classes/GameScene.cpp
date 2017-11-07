87be6b2d2fc0f75ae19856f9d3c49dad33ac0b90
kiselyovanat <kiselyov-natalya@mail.ru>
2017-11-07 10:41:06 +0700

    [+] использование класса Score в GameScene

--- Classes/GameScene.cpp
+++ Classes/GameScene.cpp
@@ -1,6 +1,6 @@
 #include <string>
 #include "GameScene.h"
-#include "ScreenConsts.h"
+#include "Score.h"
 
 USING_NS_CC;
 
@@ -9,37 +9,16 @@ Scene* GameScene::createScene()
     return GameScene::create();
 }
 
-
-void GameScene::displayScore(unsigned int score)
-{
-    auto scoreTextLabel = Label::createWithTTF("Score: ", "fonts/8458.ttf", 30);
-    scoreTextLabel->setColor(Color3B::WHITE);
-   
-    Size contentSize = scoreTextLabel->getContentSize();
-    Vec2 offset(contentSize.width / 1.8, -contentSize.height / 2.0);
-   
-    scoreTextLabel->setPosition(getLeftUpCorner() + offset);
-    this->addChild(scoreTextLabel, -1);
-
-    scoreLabel = Label::createWithTTF(std::to_string(score), "fonts/8458.ttf", 30);
-    scoreLabel->setColor(Color3B::WHITE);
-    
-    Size scoreSize = scoreLabel->getContentSize();
-    Vec2 offsetScore(scoreSize.width / 2.0 + contentSize.width , -scoreSize.height / 2.0);
-
-    scoreLabel->setPosition(getLeftUpCorner() + offsetScore); 
-    this->addChild(scoreLabel, -1);
-}
-
-
 bool GameScene::init()
 {
     if (!Scene::init()) {
         return false;
     }
 
-    score = 0;
-    displayScore(score);
+    Score *score = new Score();
+    addChild(score,0);
+
+    scheduleUpdate();
 
     return true;
 }
--- Classes/GameScene.h
+++ Classes/GameScene.h
@@ -2,17 +2,11 @@
 
 #include "cocos2d.h"
 
-class GameScene : public cocos2d::Scene
-{
+class GameScene : public cocos2d::Scene {
 public:
     static cocos2d::Scene* createScene();
 
     virtual bool init() override;
 
-    void displayScore(unsigned int score);
-    
     CREATE_FUNC(GameScene);
-private:
-    unsigned int score = 0;
-    cocos2d::Label *scoreLabel = nullptr;
 };
