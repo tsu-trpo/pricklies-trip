49fec6f0e86d8fdb39fe243381fd084910cd2337
lizatrif <lizatrif@gmail.com>
2017-11-07 15:18:14 +0700

    new function for backGround

--- Classes/GameScene.cpp
+++ Classes/GameScene.cpp
@@ -2,6 +2,7 @@
 #include "GameScene.h"
 #include "ScreenConsts.h"
 #include "CreatureRespawner.h"
+#include "BackGroundController.h"
 
 USING_NS_CC;
 
@@ -13,22 +14,22 @@ Scene* GameScene::createScene()
 void GameScene::displayScore(unsigned int score)
 {
     auto scoreTextLabel = Label::createWithTTF("Score: ", "fonts/8458.ttf", 30);
-    scoreTextLabel->setColor(Color3B::WHITE);
+    scoreTextLabel->setColor(Color3B::BLACK);
    
     Size contentSize = scoreTextLabel->getContentSize();
     Vec2 offset(contentSize.width / 1.8, -contentSize.height / 2.0);
    
     scoreTextLabel->setPosition(getLeftUpCorner() + offset);
-    this->addChild(scoreTextLabel, -1);
+    this->addChild(scoreTextLabel, 0);
 
     scoreLabel = Label::createWithTTF(std::to_string(score), "fonts/8458.ttf", 30);
-    scoreLabel->setColor(Color3B::WHITE);
+    scoreLabel->setColor(Color3B::BLACK);
     
     Size scoreSize = scoreLabel->getContentSize();
     Vec2 offsetScore(scoreSize.width / 2.0 + contentSize.width , -scoreSize.height / 2.0);
 
     scoreLabel->setPosition(getLeftUpCorner() + offsetScore); 
-    this->addChild(scoreLabel, -1);
+    this->addChild(scoreLabel, 0);
 }
 
 bool GameScene::init()
@@ -40,8 +41,12 @@ bool GameScene::init()
     score = 0;
     displayScore(score);
     
+    
+    BackGroundController *backGround = new BackGroundController(*this);
+    addChild(backGround, -1);
+    
     CreatureRespawner *respawner = new CreatureRespawner(*this);
-    addChild(respawner);
+    addChild(respawner, 0);
     
     return true;
 }
