c53ebac21b821b2d35e4ac3e5945094042893aa6
lizatrif <lizatrif@gmail.com>
2017-11-07 15:16:25 +0700

    cosmetic

--- Classes/HelloWorldScene.cpp
+++ Classes/HelloWorldScene.cpp
@@ -8,68 +8,44 @@ Scene* HelloWorld::createScene()
     return HelloWorld::create();
 }
 
-
-// on "init" you need to initialize your instance
 bool HelloWorld::init()
 {
-    //////////////////////////////
-    // 1. super init first
-    if ( !Scene::init() )
-    {
+    if ( !Scene::init() ){
         return false;
     }
 
     auto visibleSize = Director::getInstance()->getVisibleSize();
     Vec2 origin = Director::getInstance()->getVisibleOrigin();
 
-
-
     Vector<MenuItem*> MenuItems;
-    auto startItem = MenuItemImage::create("CloseNormal.png", "CloseNormal3.png",
+    
+    auto startItem = MenuItemImage::create("play2.png", "play2.png",
                                            CC_CALLBACK_1(HelloWorld::goToGameScene, this));
     startItem->setPosition(Vec2(origin.x + visibleSize.width/2,
-                                origin.y + startItem->getContentSize().height/2 + 160));
+                                origin.y + startItem->getContentSize().height/2 + 100));
     MenuItems.pushBack(startItem);
-    auto closeItem = MenuItemImage::create("CloseNormal.png", "CloseNormal2.png",
+    
+    auto closeItem = MenuItemImage::create("exit2.png", "exit2.png",
                                            CC_CALLBACK_1(HelloWorld::menuCloseCallback, this));
-    closeItem->setPosition(Vec2(origin.x + visibleSize.width - closeItem->getContentSize().width/2 ,
-                                origin.y + closeItem->getContentSize().height/2));
+    closeItem->setPosition(Vec2(origin.x + visibleSize.width - closeItem->getContentSize().width/2 - 10,
+                                origin.y + closeItem->getContentSize().height/2 + 10));
     MenuItems.pushBack(closeItem);
+    
     auto menu = Menu::createWithArray(MenuItems);
     menu->setPosition(Vec2::ZERO);
     this->addChild(menu, 1);
 
-
-    auto label = Label::createWithTTF("Pricklies trip", "fonts/8458.ttf", 50);
-    label->setPosition(Vec2(origin.x + visibleSize.width/2,
-                            origin.y + visibleSize.height - label->getContentSize().height));
-    this->addChild(label, 1);
-
-    auto mysprite = Sprite::create("HelloWorld.png");
-    mysprite->setPosition(Vec2(visibleSize.width/2 + origin.x, visibleSize.height/2 + origin.y - 70));
-    this->addChild(mysprite, 0);
+    auto backGround = Sprite::create("background.png");
+    backGround->setPosition(Vec2(visibleSize.width/2 + origin.x, visibleSize.height/2 + origin.y));
+    this->addChild(backGround, 0);
 
     return true;
 }
 
-
 void HelloWorld::menuCloseCallback(Ref* pSender)
 {
-    //Close the cocos2d-x game scene and quit the application
     Director::getInstance()->end();
-
-    #if (CC_TARGET_PLATFORM == CC_PLATFORM_IOS)
-    exit(0);
-#endif
-
-    /*To navigate back to native iOS screen(if present) without quitting the application  ,do not use Director::getInstance()->end() and exit(0) as given above,instead trigger a custom event created in RootViewController.mm as below*/
-
-    //EventCustom customEndEvent("game_scene_close_event");
-    //_eventDispatcher->dispatchEvent(&customEndEvent);
-
-
 }
-
 
 void HelloWorld::goToGameScene(Ref *sender)
 {
