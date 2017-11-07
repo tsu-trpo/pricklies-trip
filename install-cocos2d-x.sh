8213e119d30d7454f57b845231b115387a56f548
lipatova <katrinelipatova@gmail.com>
2017-11-07 11:03:11 +0700

    made few changes

--- install-cocos2d-x.sh
+++ install-cocos2d-x.sh
@@ -22,9 +22,24 @@ echo starting setup
 ./setup.py -q
 source ~/.bashrc
 
+# Add environment variable COCOS_CONSOLE_ROOT for cocos2d-x
+export COCOS_CONSOLE_ROOT="$HOME/cocos/cocos2d-x-3.15.1/tools/cocos2d-console/bin"
+export PATH=$COCOS_CONSOLE_ROOT:$PATH
+
+# Add environment variable COCOS_X_ROOT for cocos2d-x
+export COCOS_X_ROOT="$HOME/cocos"
+export PATH=$COCOS_X_ROOT:$PATH
+
+# Add environment variable COCOS_TEMPLATES_ROOT for cocos2d-x
+export COCOS_TEMPLATES_ROOT="$HOME/cocos/cocos2d-x-3.15.1/templates"
+export PATH=$COCOS_TEMPLATES_ROOT:$PATH
+
+
 echo Testing
 cd $HOME/cocos/cocos*/build
 mkdir linux-build
 cd linux-build
 cmake ../..
+
+
 
