a8b01636dca8c07ec7110ca8f8f493efcb253388
kiselyovanat <kiselyov-natalya@mail.ru>
2017-12-12 21:24:15 +0700

    тест на правильность изменения счёта

---
+++ Tests/Score.test.cpp
@@ -0,0 +1,18 @@
+#include <gtest/gtest.h>
+#include "AppDelegate.h"
+#include "Score.h"
+#include "Events.h"
+
+USING_NS_CC;
+
+TEST(Score, EventDispatcher)
+{
+  AppDelegate app;
+  app.initializeGlview();
+
+  Score *score = new Score();
+  score -> getEventDispatcher() -> setEnabled(true);
+  score -> getEventDispatcher() -> dispatchCustomEvent(eatGoodiesEvent);
+
+  ASSERT_EQ(score -> getScorePoints(), 200);
+}
