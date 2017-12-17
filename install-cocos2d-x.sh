9a0ef0bfda5e61769f568685f12d643e8652f5d2
Vadim Safonov <v.o.safonov.94@gmail.com>
2017-12-17 19:31:39 +0700

    Update install-cocos2d-x.sh

--- install-cocos2d-x.sh
+++ install-cocos2d-x.sh
@@ -10,10 +10,13 @@ rm -f $HOME/cocos/cocos2d-x-3.15.1.zip
 
 sudo cp $HOME/cocos/coc*/extern*/linux-specific/fmod/prebuilt/64-bit/libfmod.so /usr/lib
 sudo cp $HOME/cocos/coc*/extern*/linux-specific/fmod/prebuilt/64-bit/libfmodL.so /usr/lib
-ln -s libfmod.so libfmod.so.6
+ln -s /usr/lib/libfmod.so /usr/lib/libfmod.so.6
 
 cd $HOME/cocos/cocos*/build
 ./install-deps-linux.sh
+
+cd $HOME/cocos/cocos*/tools/travis-scr*/
+./install_glfw.sh
 
 cd $HOME/cocos/cocos*/
 ./setup.py -q
